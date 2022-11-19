d8f96bc1709ad42b613526c730ac47877dbdadbe
jeremiah piromgraipakd <jpiromg@gmail.com>
2022-11-18 16:10:20 -0800

    HTML/JS: save event for local storage, fully formatted HTML

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  var saveBtn = $('.saveBtn');
+  $('.saveBtn').on('click', function() {
+    let hourClickedEl = $(this).parent();
+    let hourClicked = hourClickedEl.attr('id');
+
+    let hourDesc = hourClickedEl.children('.description');
+    if ($.trim(hourDesc.val())) {
+      let eventDesc = $.trim(hourDesc.val());
+      localStorage.setItem(hourCLicked, eventDesc);
+    } else {
+      console.log('input empty');
+    }
+  })
+  
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
